0d6113bc0d2eca216051878feb71f11782af1f36
talda <talda@DESKTOP-VVOLFS2>
2016-08-31 03:12:21 +0300

    decorator design pattern step 1

--- my algorithms/src/io/MyCompressorOutputStream.java
+++ my algorithms/src/io/MyCompressorOutputStream.java
@@ -4,6 +4,18 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class MyCompressorOutputStream extends OutputStream {
+	
+	private OutputStream out;
+	
+	public MyCompressorOutputStream() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyCompressorOutputStream(OutputStream out) {
+		super();
+		this.out = out;
+	}
 
 	@Override
 	public void write(int b) throws IOException {
--- my algorithms/src/io/MyDecompressorInputStream.java
+++ my algorithms/src/io/MyDecompressorInputStream.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class MyDecompressorInputStream extends InputStream {
-
+	
+	private InputStream in;
+	
 	@Override
 	public int read() throws IOException {
 		// TODO Auto-generated method stub
