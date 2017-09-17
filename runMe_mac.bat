f3f313e4d998ead3078f8f96a79815e3366167db
Tal Darchi <taldarchi@gmail.com>
2017-09-17 16:03:19 +0300

    Add files via upload

---
+++ runMe_mac.bat
@@ -0,0 +1,1 @@
+java -classpath "myProject.jar;my algorithms.jar;myProject\swt\mac\swt.jar" boot.Run
---
+++ runMe_windows32.bat
@@ -0,0 +1,1 @@
+java -classpath "myProject.jar;my algorithms.jar;myProject\swt\windows32\swt.jar" boot.Run
